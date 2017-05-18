27e06753c48fe596f85ae9b4d653d09296516318
zhaodong1106 <1987589041@qq.com>
2017-05-18 15:06:05 +0800

    1th_commit

---
+++ com.test23/src/com/test2/domain/User2.java
@@ -0,0 +1,22 @@
+package com.test2.domain;
+
+public class User2 {
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "User2 [id=" + id + ", name=" + name + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
